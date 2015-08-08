ec992877e87d45c11877d81df782ce10c66b6938
Jared Hanson <jaredhanson@gmail.com>
2015-08-07 19:37:37 -0700

    Initial commit.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('fingro-hostmeta', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
